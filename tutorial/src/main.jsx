d725939e23a9b90386ff1679b8e3fb4fd8e653e7
Pankaj Ghosh <pankajghosh.github@gmail.com>
2024-02-13 00:31:11 +0530

    contact route done

--- tutorial/src/main.jsx
+++ tutorial/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./routes/root";
+import Contact from "./routes/contact";
 import ErrorPage from "./error-page";
 
 import "./index.css";
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+  },
+  {
+    path: "contacts/:contactId",
+    element: <Contact />,
   },
 ]);
 
