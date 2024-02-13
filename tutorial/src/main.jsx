fc18b922dc574e170fd9c09f19d6a4ef322b2a67
Pankaj Ghosh <pankajghosh.github@gmail.com>
2024-02-13 14:03:44 +0530

    add index route

--- tutorial/src/main.jsx
+++ tutorial/src/main.jsx
@@ -11,6 +11,7 @@ import Contact, { loader as contactLoader } from "./routes/contact";
 import EditContact, { action as editAction } from "./routes/edit";
 import { action as destroyAction } from "./routes/destroy";
 
+import Index from "./routes/index";
 import ErrorPage from "./error-page";
 
 import "./index.css";
@@ -23,6 +24,10 @@ const router = createBrowserRouter([
     loader: rootLoader,
     action: rootAction,
     children: [
+      {
+        index: true,
+        element: <Index />,
+      },
       {
         path: "contacts/:contactId",
         element: <Contact />,
